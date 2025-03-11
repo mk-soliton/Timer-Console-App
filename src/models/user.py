eed2c25aa2a0e04929f23d13d92cb4da685d47fb
Manoj <manoj.vijaykumar@solitontech.com>
2025-03-11 12:52:15 +0530

    feat: add user pydantic model

---
+++ src/models/user.py
@@ -0,0 +1,45 @@
+"""
+Defines User model.
+
+This module represents a user model with id, email, password and created_at
+attributes.
+"""
+
+from datetime import datetime, timezone
+from uuid import uuid4
+
+from pydantic import BaseModel, EmailStr  # type: ignore
+
+
+class User(BaseModel):
+    """
+    User Model for Application.
+
+    Represent a user model in the application using Pydantic for
+    validation."""
+    id: str
+    email: EmailStr
+    hashed_password: str
+    created_at: datetime
+
+    @classmethod
+    def create(cls, email: EmailStr, hashed_password: str) -> "User":
+        """
+        Create new user.
+
+        Creates a new User instance with a unique UUID, email, hashed password
+        and created_at timing.
+
+        Args:
+            email (EmailStr): The user's validated email.
+            hashed_password (str): The hashed password.
+
+        Returns:
+            User: A new User instance.
+        """
+        return cls(
+            id=str(uuid4()),
+            email=email,
+            hashed_password=hashed_password,
+            created_at=datetime.now(timezone.utc)
+        )

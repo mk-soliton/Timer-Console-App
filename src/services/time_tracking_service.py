fa8107a18c7e6959db49ab8996961a36a40f7e0b
Manoj <manoj.vijaykumar@solitontech.com>
2025-03-12 12:29:06 +0530

    Feature/authentication service (#1)
    * build: add dependencies
    
    * feat: add all services
    
    * feat: add custom logger
    
    * docs: add design document
    
    * docs: modify pull request template
    
    * build: add dependencies, configure settings
    
    * feat: add user pydantic model
    
    * feat: add user database logic
    
    * feat: add authentication service logic
    
    * feat: configure secret key, add custom exception
    
    * feat: modify is strong password, change datetime import
    
    * refactor: cursor execute statement
    
    * docs: update design document
    
    * build: update dependencies to compatible version
    
    * feat: remove unused functionality
    
    * feat: add entry point for the app
    
    * feat: change dir structure, set auto increment to user id
    
    * feat: modify id in the  user model
    
    * feat: add decode token functionality
    
    * feat: return token and user object
    
    * style: modify docstring
    
    * feat: change database file path, remove id from insert user query
    
    * feat: return token and user object
    
    * fix: add timestamp to resolve error in json serialization
    
    * feat: add welcome user message
    
    * style: modify docstrings
    
    * feat: remove extra files, modify docstrings
    
    * style: modify docstring

---
+++ src/services/time_tracking_service.py
@@ -0,0 +1,8 @@
+"""
+Handle Time Tracking Service.
+
+This module handles time tracking logic for the Time Tracker Console
+Application.
+It includes functions for starting, pausing, resuming, stopping timers, and
+recording timestamps for tasks.
+"""

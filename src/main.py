314a89576b967b2f7297d6912667f3abbb2dfc86
Manoj <manoj.vijaykumar@solitontech.com>
2025-03-13 17:00:38 +0530

    fix: logout functionality

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+"""Main module to run the Time Tracker application."""
+
 import inquirer  # type: ignore
 from rich.console import Console
 from rich.panel import Panel
@@ -6,9 +8,9 @@ from src.controllers.authentication_controller import AuthenticationController
 from src.controllers.task_controller import TaskController
 from src.services.category_service import CategoryService
 from src.services.task_service import TaskService
+from src.utils.helpers import clear_console
 
 # Create a Rich console instance
-
 console = Console()
 
 
@@ -19,6 +21,7 @@ def main() -> None:
     category_service = CategoryService()
     logged_in_user = None
 
+    clear_console()
     console.print(
         Panel.fit(
             "[bold magenta]Welcome to Time Tracker App[/bold magenta]",
@@ -41,30 +44,35 @@ def main() -> None:
                 break
 
             action = answers.get("action")
-
             if action == "Register":
+                clear_console()
+                console.print(
+                    Panel.fit(
+                        "[bold magenta]Register to Time Tracker App"
+                        "[/bold magenta]",
+                        title="User Registration",
+                    )
+                )
                 auth_controller.register_user()
-
             elif action == "Login":
+                clear_console()
+                console.print(
+                    Panel.fit(
+                        "[bold magenta]Login to Time Tracker App"
+                        "[/bold magenta]",
+                        title="User Login",
+                    )
+                )
                 result = auth_controller.login_user()
                 if result is None:
                     console.print("[red]Login failed. Please try again.[/red]")
                     continue
-
                 token, logged_in_user = result
-
                 if logged_in_user is None:
                     console.print("[red]Login failed. Please try again.[/red]")
                     continue
-
-                console.print(
-                    Panel.fit(
-                        f"[green]Logged in as {logged_in_user.email}[/green]",
-                        title="User Dashboard",
-                    )
-                )
-
             elif action == "Exit":
+                clear_console()
                 console.print(
                     "[bold magenta]Exiting the application. "
                     "Goodbye![/bold magenta]"
@@ -72,6 +80,7 @@ def main() -> None:
                 break
         else:
             if logged_in_user.id:
+                clear_console()
                 console.print(
                     Panel.fit(
                         f"[green]Logged in as {logged_in_user.email}[/green]",
@@ -81,9 +90,11 @@ def main() -> None:
                 task_controller = TaskController(
                     logged_in_user.id, task_service, category_service
                 )
-                result = task_controller.show_dashboard()
-                if result == "logout":  # Handle log out
-                    logged_in_user = None
+                task_controller.show_dashboard()  # Show the dashboard once
+                result = task_controller.show_task_menu()  # Show the task menu
+                if result == "logout":
+                    # Handle log out
+                    logged_in_user = None  # Reset the logged-in user
                     console.print(
                         "[bold magenta]Logged out successfully.[/bold magenta]"
                     )

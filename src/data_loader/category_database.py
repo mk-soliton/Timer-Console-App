2ce9774ebaa88662d6eea6d7318982f9080e8a43
Manoj <manoj.vijaykumar@solitontech.com>
2025-03-13 10:48:22 +0530

    feat: modify query, add docstring

--- src/data_loader/category_database.py
+++ src/data_loader/category_database.py
@@ -1,8 +1,8 @@
 """
 category_database.py module.
 
-This module handles database operations that are related to categories. This
-includes creating, and retrieve categories.
+This module handles database operations for the Category model that includes
+methods for creating, retrieving, and updating categories.
 """
 
 import sqlite3
@@ -16,6 +16,12 @@ class CategoryDatabase:
     """Database class for managing categories."""
 
     def __init__(self, db_path: str = "src/database/categories.db") -> None:
+        """Initialize the database with the path to the database file.
+
+        Args:
+            db_path (str, optional): The path to the database file.
+            Defaults to "src/database/categories.db".
+        """
         self.db_path = db_path
         self.conn: Optional[Connection] = None
         self.connect()
@@ -33,7 +39,7 @@ class CategoryDatabase:
         """Creates the categories table if it doesn't exist."""
         create_table_sql = """
         CREATE TABLE IF NOT EXISTS categories (
-            name TEXT UNIQUE NOT NULL
+            name TEXT PRIMARY KEY
         );
         """
         try:
@@ -45,7 +51,19 @@ class CategoryDatabase:
             raise Exception(f"Error creating categories table: {e}")
 
     def get_or_create_category(self, name: str) -> Category:
-        """Retrieves a category by name, or creates it if it doesn't exist."""
+        """Retrieves a category by name, or creates it if it doesn't exist.
+
+        Args:
+            name (str): The name of the category.
+
+        Raises:
+            Exception: When an error occurs while retrieving or
+            creating the category.
+            ValueError: When the category cannot be created or retrieved.
+
+        Returns:
+            Category: The category object.
+        """
         select_sql = "SELECT * FROM categories WHERE name = ?"
         insert_sql = "INSERT INTO categories (name) VALUES (?)"
         try:
@@ -54,19 +72,27 @@ class CategoryDatabase:
                 cursor.execute(select_sql, (name,))
                 row = cursor.fetchone()
                 if row:
-                    return Category(name=row["name"])
+                    return Category(
+                        name=row["name"]
+                    )  # Return existing category
 
                 cursor.execute(insert_sql, (name,))
                 self.conn.commit()
-                return Category(name=name)
-
+                return Category(name=name)  # Return newly created category
         except Error as e:
             raise Exception(f"Error retrieving or creating category: {e}")
 
         raise ValueError(f"Failed to create or retrieve category '{name}'.")
 
     def get_all_categories(self) -> List[Category]:
-        """Retrieves all categories."""
+        """Retrieve all categories.
+
+        Raises:
+            Exception: When an error occurs while retrieving categories.
+
+        Returns:
+            List[Category]: A list of category objects.
+        """
         select_sql = "SELECT * FROM categories"
         try:
             if self.conn:
--- src/data_loader/task_database.py
+++ src/data_loader/task_database.py
@@ -1,8 +1,9 @@
 """
 task_database.py module.
 
-This module handles database operations that are related to tasks. This
-includes viewing, creating, updating, and deleting tasks.
+This module contains the TaskDatabase class, which is responsible for managing
+tasks in the database. The class provides methods for saving, retrieving,
+updating, and deleting tasks from the database.
 """
 
 import sqlite3
@@ -16,42 +17,59 @@ class TaskDatabase:
     """Database class for managing tasks."""
 
     def __init__(self, db_path: str = "src/database/tasks.db") -> None:
+        """Initialize the TaskDatabase class.
+
+        Args:
+            db_path (str, optional): The path to the SQLite database file.
+            Defaults to "src/database/tasks.db".
+        """
         self.db_path = db_path
         self.conn: Optional[Connection] = None
         self.connect()
         self.create_tasks_table()
 
     def connect(self) -> None:
-        """Establishes a connection to the database."""
+        """Establish a connection to the database."""
         try:
             self.conn = sqlite3.connect(self.db_path)
             self.conn.row_factory = sqlite3.Row
-        except Error as err:
-            raise Exception(f"Error connecting to database: {err}")
+        except Error as e:
+            raise Exception(f"Error connecting to database: {e}")
 
     def create_tasks_table(self) -> None:
-        """Creates the tasks table if it doesn't exist."""
-        create_table_query = """
+        """Create the tasks table if it doesn't exist."""
+        create_table_sql = """
         CREATE TABLE IF NOT EXISTS tasks (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             user_id INTEGER NOT NULL,
             category_name TEXT NOT NULL,
             task_name TEXT NOT NULL,
             duration REAL DEFAULT 0,
-            task_status TEXT DEFAULT 'not started',
+            task_status TEXT NOT NULL,
             FOREIGN KEY (category_name) REFERENCES categories(name)
         );
         """
         try:
             if self.conn:
                 cursor = self.conn.cursor()
-                cursor.execute(create_table_query)
+                cursor.execute(create_table_sql)
                 self.conn.commit()
-        except Error as err:
-            raise Exception(f"Error creating tasks table: {err}")
+        except Error as e:
+            raise Exception(f"Error creating tasks table: {e}")
 
-    def save_task(self, task: Task) -> None:
-        """Saves a new task into the database."""
+    def save_task(self, task: Task) -> Optional[Task]:
+        """
+        Save a new task into the database.
+
+        Args:
+            task (Task): The task to be saved.
+
+        Raises:
+            Exception: When an error occurs while saving the task.
+
+        Returns:
+            Optional[Task]: The saved task with the generated ID.
+        """
         insert_sql = """
         INSERT INTO tasks (user_id, category_name, task_name, duration,
         task_status) VALUES (?, ?, ?, ?, ?)
@@ -70,11 +88,24 @@ class TaskDatabase:
                     ),
                 )
                 self.conn.commit()
-        except Error as err:
-            raise Exception(f"Error saving task: {err}")
+                task.id = cursor.lastrowid  # Set the generated ID for the task
+                return task
+            return None
+        except Error as e:
+            raise Exception(f"Error saving task: {e}")
 
     def get_tasks_by_user(self, user_id: int) -> List[Task]:
-        """Retrieves all tasks for a given user."""
+        """Retrieve all tasks for a given user.
+
+        Args:
+            user_id (int): The ID of the user.
+
+        Raises:
+            Exception: When an error occurs while retrieving tasks.
+
+        Returns:
+            List[Task]: A list of tasks for the given user.
+        """
         select_sql = "SELECT * FROM tasks WHERE user_id = ?"
         try:
             if self.conn:
@@ -92,58 +123,105 @@ class TaskDatabase:
                     )
                     for row in rows
                 ]
-        except Error as err:
-            raise Exception(f"Error retrieving tasks: {err}")
+        except Error as e:
+            raise Exception(f"Error retrieving tasks: {e}")
         return []
 
     def update_task(
         self,
         user_id: int,
         task_id: int,
+        category_name: str,
         task_name: str,
         duration: float,
     ) -> None:
-        """Update a task's category, task name, or duration for the
-        given user."""
-        check_query = "SELECT id FROM tasks WHERE id = ? AND user_id = ?"
-        update_query = """
-        UPDATE tasks
-        SET category_id = ?, task_name = ?, duration = ?
+        """Updates a task by task ID for the given user.
+
+        Args:
+            user_id (int): The ID of the user.
+            task_id (int): The ID of the task to update.
+            category_name (str): The name of the category.
+            task_name (str): The name of the task.
+            duration (float): The duration of the task.
+
+        Raises:
+            Exception: When an error occurs while updating the task.
+            Exception: When the task ID is invalid or non-existing.
+        """
+        check_sql = "SELECT COUNT(*) FROM tasks WHERE id = ? AND user_id = ?"
+        update_sql = """
+        UPDATE tasks SET category_name = ?, task_name = ?, duration = ?
         WHERE id = ? AND user_id = ?
         """
+
         try:
             if self.conn:
                 cursor = self.conn.cursor()
-                cursor.execute(check_query, (task_id, user_id))
-                task = cursor.fetchone()
-                if task is None:
-                    raise ValueError(
-                        f"Task with ID {task_id} for user {user_id} "
-                        "does not exist."
+
+                # Check if task exists
+                cursor.execute(check_sql, (task_id, user_id))
+                count = cursor.fetchone()[0]
+                if count == 0:
+                    raise Exception(
+                        f"No task found with ID {task_id} for user {user_id}."
                     )
+
+                # Proceed with the update
                 cursor.execute(
-                    update_query,
-                    (task_name, duration, task_id, user_id),
+                    update_sql,
+                    (category_name, task_name, duration, task_id, user_id),
                 )
                 self.conn.commit()
-        except Error as err:
-            raise Exception(f"Error updating task: {err}")
+        except Error as e:
+            raise Exception(f"Error updating task: {e}")
 
-    def delete_task(self, user_id: int, task_id: int) -> None:
-        """Deletes a task by task ID for the given user."""
-        check_sql = "SELECT id FROM tasks WHERE id = ? AND user_id = ?"
-        delete_sql = "DELETE FROM tasks WHERE id = ? AND user_id = ?"
+    def update_task_status(self, task_id: int, status: str) -> None:
+        """Update the status of a task.
+
+        Args:
+            task_id (int): The ID of the task to update.
+            status (str): The new status of the task.
+
+        Raises:
+            Exception: When an error occurs while updating the task status.
+        """
+        update_sql = "UPDATE tasks SET task_status = ? WHERE id = ?"
         try:
             if self.conn:
                 cursor = self.conn.cursor()
+                cursor.execute(update_sql, (status, task_id))
+                self.conn.commit()
+        except Error as e:
+            raise Exception(f"Error updating task status: {e}")
+
+    def delete_task(self, user_id: int, task_id: int) -> None:
+        """Delete a task by task ID for the given user.
+
+        Args:
+            user_id (int): The ID of the user.
+            task_id (int): The ID of the task to delete.
+
+        Raises:
+            Exception: When an error occurs while deleting the task.
+            Exception: When the task ID is invalid or non-existing.
+        """
+        check_sql = "SELECT COUNT(*) FROM tasks WHERE id = ? AND user_id = ?"
+        delete_sql = "DELETE FROM tasks WHERE id = ? AND user_id = ?"
+
+        try:
+            if self.conn:
+                cursor = self.conn.cursor()
+
+                # Check if task exists
                 cursor.execute(check_sql, (task_id, user_id))
-                task = cursor.fetchone()
-                if task is None:
-                    raise ValueError(
-                        f"Task with ID {task_id} for user {user_id} "
-                        "does not exist."
+                count = cursor.fetchone()[0]
+                if count == 0:
+                    raise Exception(
+                        f"No task found with ID {task_id} for user {user_id}."
                     )
+
+                # Proceed with deletion
                 cursor.execute(delete_sql, (task_id, user_id))
                 self.conn.commit()
-        except Error as err:
-            raise Exception(f"Error deleting task: {err}")
+        except Error as e:
+            raise Exception(f"Error deleting task: {e}")
